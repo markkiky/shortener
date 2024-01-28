af50ada5fb92f71fad921453bd1dd26859af9075
Mark Kariuki <markkaris438@gmail.com>
2024-01-28 21:10:17 +0300

    format cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 # in config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "*"
-  
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: false,
-        max_age: 86400
-    end
+  allow do
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: false,
+             max_age: 86_400
   end
+end
