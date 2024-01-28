2494fc6c5b236eb3d6418f5163df90dff1c7d6e0
Mark Kariuki <markkaris438@gmail.com>
2024-01-28 19:55:27 +0300

    change from sqlite to postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_23_201308) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.text "url"
     t.string "title"
     t.string "favicon"
